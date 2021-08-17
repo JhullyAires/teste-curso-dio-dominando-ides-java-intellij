2c0f54501d9e61c83e7dc80d97df42c6daf317c3
Jhully Aires <jhullyolires@gmail.com>
2021-08-17 12:03:50 -0300

    first commit

---
+++ src/com/company/MainJava.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import com.company.model.Gato;
+
+public class MainJava {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+/*        int a = 5;
+        int b = 3;
+        System.out.println("hello,world!" + (a+b));*/
+    }
+}
